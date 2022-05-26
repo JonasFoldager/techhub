914efd2d6eb356d3a2693b9afdcd1362ca1ab8c4
JonasFoldager <jonas.foldager11@gmail.com>
2022-05-26 09:26:03 +0200

    about

--- page-About.php
+++ page-About.php
@@ -1,46 +1,54 @@
 <?php get_header()?>
 
-
-<div class="menubar">
-<div class="logo_box">
-                <div class="logo">
-                    <a href="#index">
-                        <img id="logo-front-page" src="<?php the_field("logo")?>" alt="">
-                    </a>
-                </div>
-                <div class="menu">
-                    <?php wp_nav_menu(array("theme_location" => "techhub_green")) ?>
-                </div>
-             </div>   
-</div>
 <div style="background-color: #EBDCB1">
-    <div id="pageimg" style="background-image: url(<?php the_costum_logo("frontpage_img") ?>); clip-path: polygon(0 0, 100% 0%, 100% 100%, 0 89%);">
-        <h2 class="headline1"><?php the_field("start_ups_headline1")?></h2>
-        <h3 class="headline2"><?php the_field("start_ups_headline2")?></h3>
-        <a class="sign_up_startups" href="">Sign up</a>
+    <div id="aboutimg" style="background-image: url(<?php the_field("toppicture") ?>); clip-path: polygon(0 0, 100% 0%, 100% 100%, 0 89%);">
+   
+    <div class="aboutcoverbox">
+            <h1 class="whoarewe"><?php the_field("whoarewe")?></h1>
+            <h1 class="whatwedo"><?php the_field("whatwedo")?></h1>
+    </div>
             
     </div>
 </div>
 
+    <div class="startup_section">
+        <h3 class="startupheader"><?php the_field("aboutheader")?></h3>
+    </div>
+    <div class="boxaroundinfo">
+        <div class="about_info_box_startup">
+
+            <div class="keywordbox">
+                <p class="aboutinfotext"><?php the_field("aboutinfo")?></p>
+            </div>
+
+            <div class="keywordbox">
+                <div class="keyword"><?php the_field("keyword1")?></div>
+                <div class="keyword"><?php the_field("keyword2")?></div>
+            </div>      
+    </div>
+
+    <h3 class="startupheader"><?php the_field("missionheader")?></h3>
 
 
 
+    <div>
+        <?php
+            $loop = new WP_Query( array(
+                    'post_type' => 'citat',
+                    'posts_per_page' => -1
+                )
+                );
+                ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                  <div class="missioninfo">
+                    <div class="missiont"><?php the_title() ?></div>
+                 </div>
+                <?php endwhile; wp_reset_query(); ?>
+    </div>
 
 
 <?php get_footer()?>
+
+
+<img class="missionpicture" src="<?php the_field("missionimage") ?>" alt="">
